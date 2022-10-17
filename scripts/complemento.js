308f080224dc7d279a8776f362457d99036009ac
Hegel Pedroza <hepv12@gmail.com>
2022-10-16 20:39:41 -0500

    25 oct

--- scripts/complemento.js
+++ scripts/complemento.js
@@ -3,22 +3,22 @@ let all = [];
 let dat = amplify.store("complemento");
 let chunkIndx = [];
 
-if(dat.length ==7){
-    chunkIndx = [];
-    chunkIndx.push(4);
+if (dat.length == 7) {
+  chunkIndx = [];
+  chunkIndx.push(4);
 }
 
-if(dat.length ==8){
-    chunkIndx = [];
-    chunkIndx.push(4);
-    chunkIndx.push(5);
+if (dat.length == 8) {
+  chunkIndx = [];
+  chunkIndx.push(4);
+  chunkIndx.push(5);
 }
 
-if(dat.length ==9){
-    chunkIndx = [];
-    chunkIndx.push(3);
-    chunkIndx.push(5);
-    chunkIndx.push(6);
+if (dat.length == 9) {
+  chunkIndx = [];
+  chunkIndx.push(3);
+  chunkIndx.push(5);
+  chunkIndx.push(6);
 }
 
 // var permArr = [],
@@ -44,7 +44,7 @@ if(dat.length ==9){
 //         result = [permutation.slice()],
 //         c = new Array(length).fill(0),
 //         i = 1, k, p;
-  
+
 //     while (i < length) {
 //       if (c[i] < i) {
 //         k = i % 2 && c[i];
@@ -80,96 +80,96 @@ if(dat.length ==9){
 
 
 var swap = function (array, pos1, pos2) {
-    var temp = array[pos1];
-    array[pos1] = array[pos2];
-    array[pos2] = temp;
-  };
+  var temp = array[pos1];
+  array[pos1] = array[pos2];
+  array[pos2] = temp;
+};
 
-  var heapsPermute = function (array, n, results = []) {
-    n = n || array.length;
-    if (n === 1) {
-      results.push(array.slice());
-    } else {
-      for (var i = 1; i <= n; i += 1) {
-        heapsPermute(array, n - 1, results);
-        if (n % 2) {
-          var j = 1;
-        } else {
-          var j = i;
-        }
-        swap(array, j - 1, n - 1);
+var heapsPermute = function (array, n, results = []) {
+  n = n || array.length;
+  if (n === 1) {
+    results.push(array.slice());
+  } else {
+    for (var i = 1; i <= n; i += 1) {
+      heapsPermute(array, n - 1, results);
+      if (n % 2) {
+        var j = 1;
+      } else {
+        var j = i;
       }
+      swap(array, j - 1, n - 1);
     }
-    return results;
-  };
+  }
+  return results;
+};
 
 
-  function permutaciones(notes) {
-    var fPrR2;
-    var formaPrimaIn2;
-    var tricorde = notes;
-    var permu = [];
-    var formaPrimaCal = [];
-    var formaPrimaSel = [];
-    var formaPrimaOri = [];
+function permutaciones(notes) {
+  var fPrR2;
+  var formaPrimaIn2;
+  var tricorde = notes;
+  var permu = [];
+  var formaPrimaCal = [];
+  var formaPrimaSel = [];
+  var formaPrimaOri = [];
 
-    for (var e = 0; e < tricorde.length; e++) {
-        var tri0 = [];
-        var retro0 = [];
-        var tri0sum;
-        var tri0Max;
-        var tri0Min;
-        var retro0sum;
-        var retro0Max;
-        var retro0Min;
+  for (var e = 0; e < tricorde.length; e++) {
+    var tri0 = [];
+    var retro0 = [];
+    var tri0sum;
+    var tri0Max;
+    var tri0Min;
+    var retro0sum;
+    var retro0Max;
+    var retro0Min;
 
-        tricorde.forEach(function (nota) {
-            var nuevoValor = (nota - tricorde[e] + 12) % 12;
-            tri0.push(nuevoValor);
-            tri0.sort((a, b) => a - b);
-            // tri0sum = tri0.reduce((a, b) => a + b, 0);
-            tri0Max = Math.max(...tri0);
-            tri0Min = Math.min(...tri0);
-            tri0sum = tri0Max - tri0Min;
+    tricorde.forEach(function (nota) {
+      var nuevoValor = (nota - tricorde[e] + 12) % 12;
+      tri0.push(nuevoValor);
+      tri0.sort((a, b) => a - b);
+      // tri0sum = tri0.reduce((a, b) => a + b, 0);
+      tri0Max = Math.max(...tri0);
+      tri0Min = Math.min(...tri0);
+      tri0sum = tri0Max - tri0Min;
 
-            var nuevoValorR = (tricorde[e] - nota + 12) % 12;
-            retro0.push(nuevoValorR);
-            retro0.sort((a, b) => a - b);
-            // retro0sum = retro0.reduce((a, b) => a + b, 0);
-            retro0Max = Math.max(...retro0);
-            retro0Min = Math.min(...retro0);
-            retro0sum = retro0Max - retro0Min;
+      var nuevoValorR = (tricorde[e] - nota + 12) % 12;
+      retro0.push(nuevoValorR);
+      retro0.sort((a, b) => a - b);
+      // retro0sum = retro0.reduce((a, b) => a + b, 0);
+      retro0Max = Math.max(...retro0);
+      retro0Min = Math.min(...retro0);
+      retro0sum = retro0Max - retro0Min;
 
-        });
+    });
 
-        formaPrimaCal.push(tri0sum, retro0sum);
-        formaPrimaSel.push(tri0, retro0);
-        formaPrimaOri.push(tricorde[e].toString(), "'" + tricorde[e].toString());
-        permu.push(
-            "(" + tri0 + ")" + " " + tricorde[e].toString(),
-            "(" + retro0 + ")" + " '" + tricorde[e].toString(),
-        );
-    }
+    formaPrimaCal.push(tri0sum, retro0sum);
+    formaPrimaSel.push(tri0, retro0);
+    formaPrimaOri.push(tricorde[e].toString(), "'" + tricorde[e].toString());
+    permu.push(
+      "(" + tri0 + ")" + " " + tricorde[e].toString(),
+      "(" + retro0 + ")" + " '" + tricorde[e].toString(),
+    );
+  }
 
-    var allindx = getAllIndexes(formaPrimaCal, Math.min(...formaPrimaCal));
-    var lastcomp = [];
-    var lastcompIndx = [];
-    var lastcompVal;
-    var namIndx = []
+  var allindx = getAllIndexes(formaPrimaCal, Math.min(...formaPrimaCal));
+  var lastcomp = [];
+  var lastcompIndx = [];
+  var lastcompVal;
+  var namIndx = []
 
-    for (var i = 0; i < allindx.length; i++) {
-        lastcompVal = formaPrimaSel[allindx[i]]
-        lastcompVal = lastcompVal.reduce((a, b) => a + b, 0);
-        lastcomp.push(lastcompVal);
-        lastcompIndx.push(formaPrimaSel[allindx[i]]);
-        namIndx.push(allindx[i]);
-    }
-    var formaPrimaIndex = Math.min(...lastcomp);
-    formaPrimaIn2 = lastcompIndx[lastcomp.indexOf(formaPrimaIndex)];
+  for (var i = 0; i < allindx.length; i++) {
+    lastcompVal = formaPrimaSel[allindx[i]]
+    lastcompVal = lastcompVal.reduce((a, b) => a + b, 0);
+    lastcomp.push(lastcompVal);
+    lastcompIndx.push(formaPrimaSel[allindx[i]]);
+    namIndx.push(allindx[i]);
+  }
+  var formaPrimaIndex = Math.min(...lastcomp);
+  formaPrimaIn2 = lastcompIndx[lastcomp.indexOf(formaPrimaIndex)];
 
-    // var formaPrimaIn = formaPrimaCal.indexOf(Math.min(...formaPrimaCal));
-    fPrR2 = `(${formaPrimaIn2}) ${formaPrimaOri[namIndx[lastcomp.indexOf(formaPrimaIndex)]]}`;
-    return fPrR2;
+  // var formaPrimaIn = formaPrimaCal.indexOf(Math.min(...formaPrimaCal));
+  fPrR2 = `(${formaPrimaIn2}) ${formaPrimaOri[namIndx[lastcomp.indexOf(formaPrimaIndex)]]}`;
+  return fPrR2;
 }
 
 // const swap = (arr, i , j) =>{
@@ -191,31 +191,38 @@ var swap = function (array, pos1, pos2) {
 var perm = heapsPermute(dat);
 console.log(permutaciones(dat))
 
-for(var a=0; a < chunkIndx.length;a++){
-all = [];
-for (var j = 0; j < perm.length; j++) {
+for (var a = 0; a < chunkIndx.length; a++) {
+  all = [];
+  for (var j = 0; j < perm.length; j++) {
     var turn = perm[j]
     let chunks = _.chunk(turn, chunkIndx[a]);  ///////////
     chunks.forEach(function (e) { e.sort() });
     chunks.sort(([a, b], [c, d]) => a - c || d - b);
     all.push(chunks)
-}
+  }
 
-let lut = {},
+  let lut = {},
     red = all.filter(a => lut[a] ? false : lut[a] = true);
 
-var allFpperm = [];
-for (var i = 0; i < red.length; i++) {
+  var allFpperm = [];
+  for (var i = 0; i < red.length; i++) {
     allFpperm.push([]);
-    for (var j = 0; j < all[i].length; j++) {
-        allFpperm[i].push(permutaciones(all[i][j]));
+    for (var j = 0; j < red[i].length; j++) {
+      allFpperm[i].push(permutaciones(red[i][j]));
     }
-}
+  }
 
-let lut2 = {},
+  let lut2 = {},
     red2 = allFpperm.filter(a => lut2[a] ? false : lut2[a] = true);
 
-console.log(red2);
+  // let red2 = allFpperm
+
+  console.log(red2);
+  for(var j= 0; j<red2.length;j++){
+  d3.select("#forma").append("div")
+    .html("<a href='/compgen.html'>" + red2[j] + "</br>")
+  }
+
 }
 
 
