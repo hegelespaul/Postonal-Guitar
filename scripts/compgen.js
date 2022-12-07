6d40c184063dbdb86b288fb44bf126abe144a755
Hegel Pedroza <hepv12@gmail.com>
2022-12-07 15:19:41 -0600

    color&font

--- scripts/compgen.js
+++ scripts/compgen.js
@@ -166,36 +166,37 @@ function puntodePartida(list) {
 
 
             var dibujaDiagrama = (pisada) => {
-
+                let = adjustY = 30;
 
                 var diagramas = d3.select('.drawerDiagrama').classed("svg-container", true).append('svg');
                 diagramas.append("foreignObject")
                     .attr("width", 480)
                     .attr("height", 500)
                     .attr('x', -125)
-                    .attr('y', 90)
+                    .attr('y', 90+ adjustY)
                     .style("cursor", "pointer")
                     .append("xhtml:div")
-                    .style("font", "10px 'Helvetica Neue'")
+                    .style("font", "9px 'Helvetica Neue'")
                     .html("<button id=" + "punto" + d + " onclick=puntoPartida(" + d + "),allElements(listaF) class=" + "btnSelPoint" + ">Selecciona como punto de partida</button>");
 
                 //  .attr("preserveAspectRatio", "xMidYMid meet")
                 //  .attr("viewBox", "0 0 600 100")
                 // .classed("svg-content-responsive-diagrama", true);
 
-                // diagramas.append('text')
-                // .text((d + 1))
-                // .attr('x', 0)
-                // .attr('y', 10)
-                // .attr("font-size", "20px")
-                // .attr("font-family", "sans-serif")
-                // .attr("stroke-width", 3);
+                diagramas.append('text')
+                .text((d + 1))
+                .attr('x', 10)
+                .attr('y', adjustY - 5)
+                .attr("font-size", "20px")
+                .attr("opacity", "0.5")
+                .attr("font-family", "sans-serif")
+                .attr("stroke-width", 3);
 
                 if (fretMin > 1) {
                     diagramas.append("text")
                         .text('fr. ' + fretMin)
                         .attr('x', 6)
-                        .attr('y', 83)
+                        .attr('y', 83+ adjustY)
                         .attr("font-size", "10.5px")
                         .attr("font-family", "sans-serif")
                         .attr("stroke-width",);
@@ -204,9 +205,9 @@ function puntodePartida(list) {
                 for (var i = 0; i < 7; i++) {
                     diagramas.append('line')
                         .attr('x1', 10)
-                        .attr('y1', 10 * i)
+                        .attr('y1', 10 * i+ adjustY)
                         .attr('x2', 200)
-                        .attr('y2', 10 * i)
+                        .attr('y2', 10 * i+ adjustY)
                         .attr('stroke', 'black')
                         .attr('stroke-width', 0.3 * i);
                 }
@@ -214,34 +215,34 @@ function puntodePartida(list) {
                 for (var i = 0; i < 6; i++) {
                     diagramas.append('line')
                         .attr('x1', 10 + 38 * i)
-                        .attr('y1', 10)
+                        .attr('y1', 10+ adjustY)
                         .attr('x2', 10 + 38 * i)
-                        .attr('y2', 60.9)
+                        .attr('y2', 60.9+ adjustY)
                         .attr('stroke', 'gray')
                         .attr('stroke-width', 1);
                 }
                 if (fretMin <= 1) {
                     diagramas.append('line')
                         .attr('x1', 5)
-                        .attr('y1', 9.8)
+                        .attr('y1', 9.8+ adjustY)
                         .attr('x2', 5)
-                        .attr('y2', 60.9)
+                        .attr('y2', 60.9+ adjustY)
                         .attr('stroke', 'black')
                         .attr('stroke-width', 1);
 
                     diagramas.append('line')
                         .attr('x1', 5)
-                        .attr('y1', 10)
+                        .attr('y1', 10+ adjustY)
                         .attr('x2', 10.5)
-                        .attr('y2', 10)
+                        .attr('y2', 10+ adjustY)
                         .attr('stroke', 'black')
                         .attr('stroke-width', 0.3);
 
                     diagramas.append('line')
                         .attr('x1', 5)
-                        .attr('y1', 10 * i)
+                        .attr('y1', 10 * i+ adjustY)
                         .attr('x2', 9.5)
-                        .attr('y2', 10 * 6)
+                        .attr('y2', 10 * 6+ adjustY)
                         .attr('stroke', 'black')
                         .attr('stroke-width', 1.8);
                 }
@@ -257,7 +258,7 @@ function puntodePartida(list) {
 
                     diagramas.append('circle')
                         .attr('cx', 38 * ((Math.abs(fretMin - pisada[i][1]) % 5) + 1) - 9)
-                        .attr('cy', 10 * pisada[i][0])
+                        .attr('cy', 10 * pisada[i][0]+ adjustY)
                         .attr('r', 6)
                         .attr('stroke', 'none')
                         .attr('stroke-width', 1)
@@ -266,7 +267,7 @@ function puntodePartida(list) {
                     diagramas.append("text")
                         .text(pisada[i][2])
                         .attr('x', 38 * ((Math.abs(fretMin - pisada[i][1]) % 5) + 1) - 9)
-                        .attr('y', 10 * pisada[i][0] + 3.8)
+                        .attr('y', 10 * pisada[i][0] + 3.8+ adjustY)
                         .attr('fill', 'white')
                         .attr("font-size", "10px")
                         .attr("font-family", "sans-serif")
@@ -636,6 +637,7 @@ function allElements(list) {
         ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
         function dibujaMatrix() {
+            let adjustY =30;
 
             d3.select('.drawerDiagrama').selectAll("*").remove();
 
@@ -658,19 +660,20 @@ function allElements(list) {
                     // .attr("viewBox", "0 0 600 100")
                     // .classed("svg-content-responsive-diagrama", true);
 
-                    // diagramas.append('text')
-                    // .text((d + 1))
-                    // .attr('x', 0)
-                    // .attr('y', 10)
-                    // .attr("font-size", "20px")
-                    // .attr("font-family", "sans-serif")
-                    // .attr("stroke-width", 3);
+                    diagramas.append('text')
+                    .text((d + 1))
+                    .attr('x', 10)
+                    .attr('y', adjustY - 5)
+                    .attr("font-size", "20px")
+                    .attr("opacity", "0.5")
+                    .attr("font-family", "sans-serif")
+                    .attr("stroke-width", 3);
 
                     if (fretMin > 1) {
                         diagramas.append("text")
                             .text('fr. ' + fretMin)
                             .attr('x', 6)
-                            .attr('y', 83)
+                            .attr('y', 83+ adjustY)
                             .attr("font-size", "10.5px")
                             .attr("font-family", "sans-serif")
                             .attr("stroke-width", 1);
@@ -679,9 +682,9 @@ function allElements(list) {
                     for (var i = 0; i < 7; i++) {
                         diagramas.append('line')
                             .attr('x1', 10)
-                            .attr('y1', 10 * i)
+                            .attr('y1', 10 * i+ adjustY)
                             .attr('x2', 200)
-                            .attr('y2', 10 * i)
+                            .attr('y2', 10 * i+ adjustY)
                             .attr('stroke', 'black')
                             .attr('stroke-width', 0.3 * i);
                     }
@@ -689,34 +692,34 @@ function allElements(list) {
                     for (var i = 0; i < 6; i++) {
                         diagramas.append('line')
                             .attr('x1', 10 + 38 * i)
-                            .attr('y1', 10)
+                            .attr('y1', 10+ adjustY)
                             .attr('x2', 10 + 38 * i)
-                            .attr('y2', 60.9)
+                            .attr('y2', 60.9+ adjustY)
                             .attr('stroke', 'gray')
                             .attr('stroke-width', 1);
                     }
                     if (fretMin <= 1) {
                         diagramas.append('line')
                             .attr('x1', 5)
-                            .attr('y1', 9.8)
+                            .attr('y1', 9.8+ adjustY)
                             .attr('x2', 5)
-                            .attr('y2', 60.9)
+                            .attr('y2', 60.9+ adjustY)
                             .attr('stroke', 'black')
                             .attr('stroke-width', 1);
 
                         diagramas.append('line')
                             .attr('x1', 5)
-                            .attr('y1', 10)
+                            .attr('y1', 10+ adjustY)
                             .attr('x2', 10.5)
-                            .attr('y2', 10)
+                            .attr('y2', 10+ adjustY)
                             .attr('stroke', 'black')
                             .attr('stroke-width', 0.3);
 
                         diagramas.append('line')
                             .attr('x1', 5)
-                            .attr('y1', 10 * i)
+                            .attr('y1', 10 * i+ adjustY)
                             .attr('x2', 9.5)
-                            .attr('y2', 10 * 6)
+                            .attr('y2', 10 * 6+ adjustY)
                             .attr('stroke', 'black')
                             .attr('stroke-width', 1.8);
                     }
@@ -735,7 +738,7 @@ function allElements(list) {
 
                         diagramas.append('circle')
                             .attr('cx', 38 * ((Math.abs(fretMin - pisada[i][1]) % 5) + 1) - 9)
-                            .attr('cy', 10 * pisada[i][0])
+                            .attr('cy', 10 * pisada[i][0]+ adjustY)
                             .attr('r', 6)
                             .attr('stroke', 'none')
                             .attr('stroke-width', 1)
@@ -744,7 +747,7 @@ function allElements(list) {
                         diagramas.append("text")
                             .text(pisada[i][2])
                             .attr('x', 38 * ((Math.abs(fretMin - pisada[i][1]) % 5) + 1) - 9)
-                            .attr('y', 10 * pisada[i][0] + 3.8)
+                            .attr('y', 10 * pisada[i][0] + 3.8+ adjustY)
                             .attr('fill', 'white')
                             .attr("font-size", "10px")
                             .attr("font-family", "sans-serif")
